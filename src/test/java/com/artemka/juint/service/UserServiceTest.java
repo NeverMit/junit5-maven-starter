a30a36e54bd4b62ab888cc3c2bf8e615b8768df2
NeverMit <markovnikov96@mail.ru>
2024-01-22 17:04:44 +0500

    Initial commit

---
+++ src/test/java/com/artemka/juint/service/UserServiceTest.java
@@ -0,0 +1,11 @@
+package com.artemka.juint.service;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class UserServiceTest {
+    @Test
+    void test(){
+        Assertions.assertTrue(false);
+    }
+}

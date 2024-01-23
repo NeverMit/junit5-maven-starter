23358d76a84ffa9caf36872baafa87b0a3721c4f
NeverMit <markovnikov96@mail.ru>
2024-01-23 14:22:33 +0500

    Test LifeCycle. Используем @TestInstance(TestInstance.Lifecycle.PER_METHOD)

--- src/test/java/com/artemka/juint/service/UserServiceTest.java
+++ src/test/java/com/artemka/juint/service/UserServiceTest.java
@@ -2,30 +2,40 @@ package com.artemka.juint.service;
 
 import com.artemka.junit.dto.User;
 import com.artemka.junit.service.UserService;
-import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.*;
 
-public class UserServiceTest {
+@TestInstance(TestInstance.Lifecycle.PER_METHOD)
+class UserServiceTest {
+    private UserService userService;
+    @BeforeAll
+    static void init(){
+        System.out.println("Before all: ");
+    }
     @BeforeEach
     void prepare(){
         System.out.println("Before each: "+this);
+        userService=new UserService();
     }
     @Test
     void usersEmptyIfNoUsersAdded(){
         System.out.println("Test 1: "+this);
-        var userService=new UserService();
         var users=userService.getAll();
         Assertions.assertTrue(users.isEmpty(),()->"User list should be empty");
     }
     @Test
     void usersSizeIfUsersAdded(){
         System.out.println("Test 2: "+this);
-        var userService=new UserService();
         userService.add(new User());
         userService.add(new User());
-
         var users=userService.getAll();
         Assertions.assertEquals(2,users.size());
+    }
+    @AfterEach
+    void deleteDataFromDatabase(){
+        System.out.println("After each: "+this);
+    }
+    @AfterAll
+    static void closeConnectionPool(){
+        System.out.println("After all: ");
     }
 }
